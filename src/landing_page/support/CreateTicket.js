dcb6c535546f0988b84664011e18d2f5fdc1d97a
Ajay Saini <153486378+ajaysaini3078@users.noreply.github.com>
2024-07-14 23:14:41 +0530

    add support page

--- src/landing_page/support/CreateTicket.js
+++ src/landing_page/support/CreateTicket.js
@@ -1,11 +1,192 @@
-import React from 'react';
+import React from "react";
 
 function CreateTicket() {
-  return ( 
-    <h3>
-      CreateTicket
-    </h3>
-   );
+  return (
+    <div className="container">
+      <div className="row">
+        <p className="fs-3">To create a ticket, select a relevant topic</p>
+        <div className="col-4 p-3">
+          <a href="">
+            <h1 className="fs-4 text-dark">
+              <i class="fa-solid fa-circle-plus fs-5"></i>&nbsp;&nbsp;Account
+              Opening
+            </h1>
+          </a>
+          <a href="" className="lh-lg ms-4">
+            Getting started
+          </a>
+          <br />
+          <a href="" className="lh-lg ms-4">
+            Online
+          </a>
+          <br />
+          <a href="" className="lh-lg ms-4">
+            Offline
+          </a>
+          <br />
+          <a href="" className="lh-lg ms-4">
+            Charges
+          </a>
+          <br />
+          <a href="" className="lh-lg ms-4">
+            Company, Partnership and HUF
+          </a>
+          <br />
+          <a href="" className="lh-lg ms-4">
+            Non Resident Indian (NRI)
+          </a>
+          <br />
+        </div>
+        <div className="col-4 p-3">
+          <a href="">
+            <h1 className="fs-4 text-dark">
+              <i class="fa-regular fa-user fs-5"></i>&nbsp;&nbsp;Your Zerodha
+              Account
+            </h1>
+          </a>
+          <a href="" className="lh-lg ms-4">
+            Login credentials
+          </a>
+          <br />
+          <a href="" className="lh-lg ms-4">
+            Your Profile
+          </a>
+          <br />
+          <a href="" className="lh-lg ms-4">
+            Account modification and segment addition
+          </a>
+          <br />
+          <a href="" className="lh-lg ms-4">
+            CMR & DP ID
+          </a>
+          <br />
+          <a href="" className="lh-lg ms-4">
+            Nomination
+          </a>
+          <br />
+          <a href="" className="lh-lg ms-4">
+            Transfer and conversion of shares
+          </a>
+          <br />
+        </div>
+        <div className="col-4 p-3">
+          <a href="">
+            <h1 className="fs-4 text-dark">
+              <i class="fa-solid fa-chart-simple fs-5"></i>&nbsp;&nbsp;Your
+              Trading and Markets
+            </h1>
+          </a>
+          <a href="" className="lh-lg ms-4">
+            Trading FAQs
+          </a>
+          <br />
+          <a href="" className="lh-lg ms-4">
+            Kite
+          </a>
+          <br />
+          <a href="" className="lh-lg ms-4">
+            Margins
+          </a>
+          <br />
+          <a href="" className="lh-lg ms-4">
+            Product and order types
+          </a>
+          <br />
+          <a href="" className="lh-lg ms-4">
+            Corporate actions
+          </a>
+          <br />
+          <a href="" className="lh-lg ms-4">
+            Kite features
+          </a>
+          <br />
+        </div>
+        <div className="col-4 mt-3 p-3">
+          <a href="">
+            <h1 className="fs-4 text-dark">
+              <i class="fa-regular fa-envelope fs-5"></i>&nbsp;&nbsp;Funds
+            </h1>
+          </a>
+          <a href="" className="lh-lg ms-4">
+            Fund withdrawal
+          </a>
+          <br />
+          <a href="" className="lh-lg ms-4">
+            Adding funds
+          </a>
+          <br />
+          <a href="" className="lh-lg ms-4">
+            Adding bank accounts
+          </a>
+          <br />
+          <a href="" className="lh-lg ms-4">
+            eMandates
+          </a>
+          <br />
+        </div>
+        <div className="col-4 mt-3 p-3">
+          <a href="">
+            <h1 className="fs-4 text-dark">
+              <i class="fa-regular fa-circle-user fa-rotate-270"></i>
+              &nbsp;&nbsp;Console
+            </h1>
+          </a>
+          <a href="" className="lh-lg ms-4">
+            IPO
+          </a>
+          <br />
+          <a href="" className="lh-lg ms-4">
+            Portfolio
+          </a>
+          <br />
+          <a href="" className="lh-lg ms-4">
+            Funds statement
+          </a>
+          <br />
+          <a href="" className="lh-lg ms-4">
+            Profile
+          </a>
+          <br />
+          <a href="" className="lh-lg ms-4">
+            Reports
+          </a>
+          <br />
+          <a href="" className="lh-lg ms-4">
+            Referral program
+          </a>
+          <br />
+        </div>
+        <div className="col-4 mt-3 p-3">
+        <a href="">
+            <h1 className="fs-4 text-dark">
+            <i class="fa-regular fa-circle"></i>
+              &nbsp;&nbsp;Coin
+            </h1>
+          </a>
+          <a href="" className="lh-lg ms-4">
+          Understanding mutual funds and Coin
+          </a>
+          <br />
+          <a href="" className="lh-lg ms-4">
+          Coin app
+          </a>
+          <br />
+          <a href="" className="lh-lg ms-4">
+          Coin web
+          </a>
+          <br />
+          <a href="" className="lh-lg ms-4">
+          Transactions and reports
+          </a>
+          <br />
+          <a href="" className="lh-lg ms-4">
+          National Pension Scheme (NPS)
+          </a>
+          <br />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CreateTicket;
--- src/landing_page/support/Hero.js
+++ src/landing_page/support/Hero.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React from "react";
 
 function Hero() {
-  return ( 
-    <h3>
-      Hero
-    </h3>
-   );
+  return (
+    <section className="container-fluid mb-5 pb-5" id="supportBox">
+      <div className="p-5 mt-5" id="supportWrapper">
+        <a href="" style={{ color: "white" }} className="fs-4">
+          Support Portal
+        </a>
+        <a href="" style={{ color: "white" }} className="fs-5">
+          Track tickets
+        </a>
+      </div>
+      <div className="row px-5">
+        <div className="col-6 p-3">
+          <h2>Search for an answer or browse help topics to create a ticket</h2>
+          <div class="input-group mb-3 bg-white mt-4 rounded">
+            <input
+              type="text"
+              class="form-control p-3"
+              placeholder="Eg: how do i activate F&O, why is my order getting rejected ..."
+            />
+            <button
+              class="btn"
+            >
+              <i class="fa-solid fa-magnifying-glass"></i>
+            </button>
+          </div>
+          <a href="" style={{ color: "white" }}>Track account opening </a> 
+          <a href=""  style={{ color: "white" }}>&nbsp;&nbsp;Track segment activation </a>
+          <a href=""  style={{ color: "white" }}>&nbsp;&nbsp;Intraday margins </a><br />
+          <a href=""  style={{ color: "white" }}>Kite user manual</a>
+        </div>
+        <div className="col-6 p-5">
+        <h2>Featured</h2>
+        <p>Due to a system upgrade, HDFC Bank is experiencing a global downtime from 7 pm 12th July to 4:30 pm 13th July. HDFC UPI and net banking payments may not be available, and direct transfers using NEFT, IMPS, and RTGS may be delayed. <a href=""  style={{ color: "white" }}>Learn more.</a></p>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Hero;
